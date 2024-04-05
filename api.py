dbe20fc4e6fe78094b7bc630fa6a404fc65e18a6
talitta-nunes <talitta-nunes@github.com>
2024-04-05 14:27:20 -0300

    feat:improving data

--- api.py
+++ api.py
@@ -7,61 +7,68 @@ from sqlalchemy import text, inspect
 
 def add_data_to_database():
     inspector = inspect(engine)
-    table_exists = inspector.has_table('sgp_all_data')
+    table_exists = inspector.has_table('sgp_iron_data')
     if not table_exists:
-        df = pd.read_json("responseSGP.json")
-        df.to_sql('sgp_all_data', con=engine, index=False)
+        df = pd.read_json("responseIronData.json")
+        df.to_sql('sgp_iron_data', con=engine, index=False)
     else:
-        print("Table 'sgp_all_data' already exists. Not appending or replacing.")
+        print("Table 'sgp_iron_data' already exists. Not appending or replacing.")
 
 
-def retrieve_data_from_database():
-    with engine.connect() as conn:
-        results = conn.execute(text("""
-            SELECT
-                `TOC (wt%)`,
-                `interpreted age`
-            FROM
-                sgp_all_data;                        
-        """)).fetchall()
-        result_data = np.array(results)
-        result = result_data.astype(float)
+# def retrieve_data_from_database():
+#     with engine.connect() as conn:
+#         results = conn.execute(text("""
+#             SELECT
+#                 `TOC (wt%)`,
+#                 `interpreted age`
+#             FROM
+#                 sgp_all_data;                        
+#         """)).fetchall()
+#         result_data = np.array(results)
+#         result = result_data.astype(float)
 
-    return result
+#     return result
        
-def perform_sindy_analysis(result):
-    import numpy as np
-    import pandas as pd
-    import matplotlib.pyplot as plt
-    from pysindy import SINDy, STLSQ
+# def perform_sindy_analysis(result):
+#     import numpy as np
+#     import pandas as pd
+#     import matplotlib.pyplot as plt
+#     from pysindy import SINDy, STLSQ
   
     
-    df = pd.DataFrame(result, columns=['toc',  'age'])
-    df = df.sort_values(by='age')
+#     df = pd.DataFrame(result, columns=['toc',  'age'])
+#     df = df.sort_values(by='age')
+  
+
+#     df['toc'] = df['toc'].interpolate(method='linear')
+#     df['age'] = df['age'].interpolate(method='linear')
     
-    df['toc'] = df['toc'].interpolate(method='linear')
-    df['age'] = df['age'].interpolate(method='linear')
+    
+#     print('\nFinal DataFrame:')
+#     print(df)
+#     mean_toc = df.groupby('age')["toc"].mean().reset_index()
 
-    mean_toc = df.groupby('age')["toc"].mean().reset_index()
+#     t = mean_toc["age"].values
+#     toc_data = mean_toc["toc"].values
+#     print(f'\neste é o t', t)
+#     print(f'------------------------fim----------------------')
+#     print(f'\neste é o toc', toc_data)
+#     print(f'------------------------fim----------------------')
+#     feature_names = ["toc"]
+#     custom_optimizer = STLSQ(threshold=0)  
+#     model = SINDy(optimizer=custom_optimizer, feature_names=feature_names)
+#     model.fit(toc_data, t=t)
+#     model.print()
 
-    t = mean_toc["age"].values
-    toc_data = mean_toc["toc"].values
+#     predicted_toc = model.predict(toc_data)
 
-    feature_names = ["toc"]
-    custom_optimizer = STLSQ(threshold=0)  
-    model = SINDy(optimizer=custom_optimizer, feature_names=feature_names)
-    model.fit(toc_data, t=t)
-    model.print()
-
-    predicted_toc = model.predict(toc_data)
-
-    plt.plot(t, toc_data, label='Original toc')
-    plt.plot(t, predicted_toc, label='Predicted toc', linestyle='--', color='red')
-    plt.gca().invert_xaxis()
-    plt.xlabel('Time (Ma)')
-    plt.ylabel('TOC (wt%)')
-    plt.legend()
-    plt.show()
+#     plt.plot(t, toc_data, label='Original toc')
+#     plt.plot(t, predicted_toc, label='Predicted toc', linestyle='--', color='red')
+#     plt.gca().invert_xaxis()
+#     plt.xlabel('Time (Ma)')
+#     plt.ylabel('TOC (wt%)')
+#     plt.legend()
+#     plt.show()
 
 
 
@@ -71,8 +78,8 @@ def perform_sindy_analysis(result):
 add_data_to_database()
 
 # Retrieve data from the database
-result = retrieve_data_from_database()
-perform_sindy_analysis(result)
+# result = retrieve_data_from_database()
+# perform_sindy_analysis(result)
 
 
 
--- constants.py
+++ constants.py
@@ -3,11 +3,21 @@ URL =  'https://sgp-search.io/api/v1/post'
 #variables
 country_name = ["Antarctica","Atlantic","Argentina","Australia","Austria","Barents Sea","Belgium","Black Sea","Bolivia","Brazil","Caribbean Sea","Canada","Chile","China","Colombia","Denmark","Czech Republic","Egypt","Ethiopia","Estonia","Finland","Gabon","Ghana","Germany","France","Greenland","Guyana","Gulf of Mexico","Indian","India","Japan","Korea, South","Latvia","Jordan","Italy","Indonesia","Libya","Mauritania","Madagascar","Mexico","Mediterranean Sea","Morocco","New Caledonia","New Zealand","Namibia","Mongolia","North America","North Pacific","Oman","North Atlantic","Pacific","Norway","Pakistan","Poland","Red Sea","Russia","Senegal","South Africa","South Atlantic","South Pacific","Spain","Sri Lanka","Svalbard and Jan Mayen Islands","Sweden","Switzerland","Tanzania","Ukraine","United States","United Kingdom","Zimbabwe","Venezuela"]
 fields_to_show = [  
-    "toc",
-    "fe_hr_fe_t",
-    "interpreted_age",
-    "os187_os188i",
-    "country",
+   "country",
+		"nd", 
+		"mo", 
+		"v",
+		"u",
+		"su",
+		"toc",
+		"fe_hr_fe_t",
+        "fe_py_fe_hr",
+		"interpreted_age",
+		"tic",
+		"del_13c_org",
+		"del_13c_carb",
+    	"p",
+    	"tot_c"
 ]
 
 
